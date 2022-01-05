5db83373e0fb3e0b4da555eb47c5030d04310511
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2022-01-05 11:24:15 -0700

    Instance of competition update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Carter                                                    */
-/*    Created:      Tue Dec 14 2021                                           */
-/*    Description:  V5 project                                                */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------*/
+/*Refrences for Carter                                                                 */
+/*https://api.vexcode.cloud/v5/                                                        */
+/*https://content.vexrobotics.com/docs/21-22/tipping-point/2021-VRC-GameManual-2.3.pdf */
+/*                                                                                     */
+/*                                                                                     */
+/*                                                                                     */
+/*-------------------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
@@ -22,44 +31,68 @@
 #include "vex.h"
 using namespace vex;
 
+
   int pneumaticStatus = 0;
   int pneumaticStatusBack = 0;
   int powerStatus = 0;
   int actPowerStatus = 0;
   int driveDirection = 0;
 
-void pre_auton(void){
-}
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  }
 // Battery Percent LED Code
   int BatteryPercent(){
-    int currentPercent = Brain.Battery.capacity();
-      if (currentPercent > 51 ) {
-      RedLED.set(true);
-      YellowLED.set(true);
-      RedLED.set(true);
-      }
-      else if(currentPercent > 25) {
-      YellowLED.set(true);
-      RedLED.set(true);
-      }
-      else if(currentPercent > 10){
-      RedLED.set(true);
-      }
-      vex::task::sleep(20);
+    while(true){
+      int currentPercent = Brain.Battery.capacity();
+        if (currentPercent > 51 ) {
+        RedLED.set(true);
+        YellowLED.set(true);
+        RedLED.set(true);
+        }
+        else if(currentPercent > 25) {
+        RedLED.set(false);
+        YellowLED.set(true);
+        RedLED.set(true);
+        }
+        else if(currentPercent > 10){
+        RedLED.set(false);
+        YellowLED.set(false);
+        RedLED.set(true);
+        }
+        vex::task::sleep(20);
+    }
     return 1;
   }
 
 
 
-void autonomous(void){
+
+void autonomous(void) {
   vex::task Battery(BatteryPercent);
-}
-// Start of usercontrol code
-
-  int CustumController(){
-  // MAX (X,Y) = (479,239)
-    return 1;
   }
+// Start of usercontrol code
+  // Start of CustumController code
+    int CustumController(){
+    // MAX (X,Y) = (479,239)
+      return 1;
+    }
     // Functions
     
     void PneumaticSwitch()
@@ -99,19 +132,27 @@ void autonomous(void){
       }
     }
     // End of usercontrol code
-void usercontrol(void){
-  Controller1.ButtonX.pressed(PneumaticSwitch);
-  Controller1.ButtonA.pressed(PneumaticSwitchBack);
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (true) {
+    Controller1.ButtonX.pressed(PneumaticSwitch);
+    Controller1.ButtonA.pressed(PneumaticSwitchBack);
+    vex::task Battery(BatteryPercent);
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
 }
-//launch via the main class 
+
 int main() {
   // Set up callbacks for autonomous and driver control periods.
-  competition.autonomous(autonomous);
-  competition.drivercontrol(usercontrol); 
-  // Run the preautonomous function
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+  // Run the pre-autonomous function.
   pre_auton();
-  // Never ending timer
-  waitUntil(false);
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
 }

15525047d5ac45a8e011a38175f53b9054473791
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2022-01-05 01:45:03 -0700

    Update with LED ect.

--- src/main.cpp
+++ src/main.cpp
@@ -13,72 +13,105 @@
 // Controller1          controller                    
 // Drivetrain           drivetrain    1, 2            
 // Pneumatic            digital_out   A               
+// PneumaticBack        digital_out   B               
+// RedLED               digital_out   H               
+// YellowLED            digital_out   G               
+// GreenLED             digital_out   F               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
-
 using namespace vex;
 
   int pneumaticStatus = 0;
+  int pneumaticStatusBack = 0;
   int powerStatus = 0;
   int actPowerStatus = 0;
   int driveDirection = 0;
 
-  // Functions
-  
-  void PneumaticSwitch()
-  {
-    switch(pneumaticStatus)
-    {
-      case 0:
-      {
-        pneumaticStatus = 1;
-        Pneumatic.set(true);
-        break;
+void pre_auton(void){
+}
+// Battery Percent LED Code
+  int BatteryPercent(){
+    int currentPercent = Brain.Battery.capacity();
+      if (currentPercent > 51 ) {
+      RedLED.set(true);
+      YellowLED.set(true);
+      RedLED.set(true);
       }
-      case 1:
-      {
-        pneumaticStatus = 0;
-        Pneumatic.set(false);
-        break;
+      else if(currentPercent > 25) {
+      YellowLED.set(true);
+      RedLED.set(true);
       }
-    }
+      else if(currentPercent > 10){
+      RedLED.set(true);
+      }
+      vex::task::sleep(20);
+    return 1;
   }
 
-void Power()
-  {
-    switch(powerStatus)
+
+
+void autonomous(void){
+  vex::task Battery(BatteryPercent);
+}
+// Start of usercontrol code
+
+  int CustumController(){
+  // MAX (X,Y) = (479,239)
+    return 1;
+  }
+    // Functions
+    
+    void PneumaticSwitch()
     {
-      case 0:
+      switch(pneumaticStatus)
       {
-        powerStatus = 1;
-        Controller1.Screen.clearLine(3); 
-        Controller1.Screen.print("Default Speed");
-        Drivetrain.setDriveVelocity(75, percent);
-        actPowerStatus = 75;
-        break;
-      }
-      case 1:
-      {
-        powerStatus = 0;
-        Controller1.Screen.clearLine(3); 
-        Controller1.Screen.print("Speed");
-        Drivetrain.setDriveVelocity(100, percent);
-        actPowerStatus = 75;
-        break;
+        case 0:
+        {
+          pneumaticStatus = 1;
+          Pneumatic.set(true);
+          break;
+        }
+        case 1:
+        {
+          pneumaticStatus = 0;
+          Pneumatic.set(false);
+          break;
+        }
       }
     }
-  }
-
-//launch via the main class 
-int main() 
-{
-  Brain.Screen.clearScreen();
-  Brain.Screen.print("DEBUG: Teleop\n");
-  Drivetrain.setDriveVelocity(75, percent);  
+    void PneumaticSwitchBack()
+    {
+      switch(pneumaticStatusBack)
+      {
+        case 0:
+        {
+          pneumaticStatusBack = 1;
+          PneumaticBack.set(true);
+          break;
+        }
+        case 1:
+        {
+          pneumaticStatusBack = 0;
+          PneumaticBack.set(false);
+          break;
+        }
+      }
+    }
+    // End of usercontrol code
+void usercontrol(void){
   Controller1.ButtonX.pressed(PneumaticSwitch);
-  Controller1.ButtonUp.pressed(Power);
-
+  Controller1.ButtonA.pressed(PneumaticSwitchBack);
+}
+//launch via the main class 
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  competition.autonomous(autonomous);
+  competition.drivercontrol(usercontrol); 
+  // Run the preautonomous function
+  pre_auton();
+  // Never ending timer
+  waitUntil(false);
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 }
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -13,6 +13,10 @@ motor LeftDriveSmart = motor(PORT1, ratio18_1, false);
 motor RightDriveSmart = motor(PORT2, ratio18_1, true);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
 digital_out Pneumatic = digital_out(Brain.ThreeWirePort.A);
+digital_out PneumaticBack = digital_out(Brain.ThreeWirePort.B);
+digital_out RedLED = digital_out(Brain.ThreeWirePort.H);
+digital_out YellowLED = digital_out(Brain.ThreeWirePort.G);
+digital_out GreenLED = digital_out(Brain.ThreeWirePort.F);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
